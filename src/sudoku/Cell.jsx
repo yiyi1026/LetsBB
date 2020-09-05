fb8f40bfeace73c53bc1f736e4cd3a8df6d22754
dereksun <derek.y.sun@gmail.com>
2020-09-04 19:16:24 -0700

    cell select

--- src/sudoku/Cell.jsx
+++ src/sudoku/Cell.jsx
@@ -3,8 +3,13 @@ import { Box } from '@material-ui/core'
 
 export default function Cell(props) {
     let value = props.value
+    let onClick = props.onClick
+    let bgcolor = "white"
+    if(props.selected){
+        bgcolor = "primary.main"
+    }
     return (
-        <Box width={1/9} justifyContent="center">
+        <Box width={1/9} onClick={onClick} bgcolor={bgcolor}>
             {value}
         </Box>
     )
--- src/sudoku/Sudoku.jsx
+++ src/sudoku/Sudoku.jsx
@@ -1,21 +1,49 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Cell from './Cell'
 import { Box } from '@material-ui/core'
 
 export default function Sudoku(props) {
 
+    const generateGame = () => {
+        return [
+            [1,2,3,4,5,6,7,8,9],
+            [1,2,3,4,5,6,7,8,9],
+            [1,2,3,4,5,6,7,8,9],
+            [1,2,3,4,5,6,7,8,9],
+            [1,2,3,4,5,6,7,8,9],
+            [1,2,3,4,5,6,7,8,9],
+            [1,2,3,4,5,6,7,8,9],
+            [1,2,3,4,5,6,7,8,9],
+            [1,2,3,4,5,6,7,8,9]
+        ]
+    }
+
+    const game = generateGame()
+
+    const [selected,setSelected] = useState([8, 7])
+
+    const selectCell = (x,y) => {
+        setSelected([x,y])
+    } 
+
     return (
         <div width="100%">
             <Box width="100%" display="flex" flexDirection="column">
-                {[...Array(9).keys()].map(r => {
-                    
-                    return <Box key={"row"+r} display="flex" flexDirection="row" width="100%"  justifyContent="center">
-                        {[...Array(9).keys()].map(c => {
-                            return <Cell className="column" key={c} x={r} y={c} value={2}/>
+                {[...Array(9).keys()].map(x => {
+                    return <Box key={"row"+x} display="flex" flexDirection="row" width="100%"  justifyContent="center">
+                        {[...Array(9).keys()].map(y => {
+                            return <Cell className="column" 
+                                    key={y} 
+                                    x={x} 
+                                    y={y} 
+                                    value={game[x][y]}
+                                    onClick={() => { selectCell(x, y); }}
+                                    selected= {x === selected[0] && y === selected[1]}
+                                    />
                         })}
                     </Box>
                 })}
             </Box>
-                </div>
+        </div>
     );
 }
