c274865407c280506c79b0444b1982b2927ff9f3
yiyi1026 <vivian.li.uibe@gmail.com>
2020-09-04 19:06:18 -0700

    sudoku layout

--- src/sudoku/Cell.jsx
+++ src/sudoku/Cell.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
 import { Box } from '@material-ui/core'
+import React from 'react'
 
 export default function Cell(props) {
-    let value = props.value
-    return (
-        <Box width={1/9} justifyContent="center">
-            {value}
-        </Box>
-    )
+  const { value } = props
+  return (
+    <Box display="flex" width={1 / 9} justifyContent="center" alignItems="center">
+      {value}
+
+    </Box>
+  )
 }
--- src/sudoku/Sudoku.jsx
+++ src/sudoku/Sudoku.jsx
@@ -1,21 +1,18 @@
-import React from 'react'
-import Cell from './Cell'
 import { Box } from '@material-ui/core'
+import React from 'react'
+
+import Cell from './Cell'
 
 export default function Sudoku(props) {
-
-    return (
-        <div width="100%">
-            <Box width="100%" display="flex" flexDirection="column">
-                {[...Array(9).keys()].map(r => {
-                    
-                    return <Box key={"row"+r} display="flex" flexDirection="row" width="100%"  justifyContent="center">
-                        {[...Array(9).keys()].map(c => {
-                            return <Cell className="column" key={c} x={r} y={c} value={2}/>
-                        })}
-                    </Box>
-                })}
-            </Box>
-                </div>
-    );
+  return (
+    <div width="100%" style={{ height: 600 }}>
+      <Box style={{ width: 500, height: 450 }} display="flex" flexDirection="column">
+        {[...Array(9).keys()].map((r) => (
+          <Box key={`row${r}`} display="flex" flexDirection="row" width="100%" height="100%" justifyContent="center">
+            {[...Array(9).keys()].map((c) => <Cell className="column" key={c} x={r} y={c} value={2} />)}
+          </Box>
+        ))}
+      </Box>
+    </div>
+  );
 }
